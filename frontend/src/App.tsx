9b5a91fb95f21a90b6c035cae5de0fe64739f54b
Surien Das-Giwojno <surien.das-giwojno@uwaterloo.ca>
2021-08-10 23:38:53 -0400

    Initial frontend setup (#58)
    * Initial frontend setup
    
    * Add missing package.json file
    
    * Update gitignore
    
    * Add tsconfig file
    
    * Add missing package lock file

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './css/App.scss';
+
+
+class App extends Component<{}> {
+
+  render() {
+    console.log("app rendered");
+    return (
+      <div className="App">
+
+      </div>
+      
+    );
+  }
+}
+
+
+
+export default App;
